e5f1f5216bac27e3be30327ed205e63cc9377bfd
Nicholas Hyatt <nhyatt@wisc.edu>
2016-03-15 10:03:31 -0500

    basic chat

--- www/js/controller.js
+++ www/js/controller.js
@@ -5,40 +5,28 @@ angular.module('exisChat.controller', [])
   $scope.msgs = [];
 
   //Login anonymously to Exis (requires Auth appliance level 0)
-  $riffle.login().then(join);
+  $riffle.login();
 
-  //a handler that only wants message objects
-  var wantMsg = $riffle.want(receiveMsg, {username: String, msg: String});
-
-  //a handler that only wants join events
-  var wantJoinEvent = $riffle.want(joinEvent, String);
+  //subscribe to the chat channel
+  $riffle.subscribe('exisChat', gotMsg);
 
   //handle messages here
-  function receiveMsg(msg){
+  function gotMsg(msg){
     msg.received = true;
-    $scope.msgs.push(msg);
-    $ionicScrollDelegate.$getByHandle('mainScroll').scrollBottom();
-  }
-
-  //handle join events
-  function joinEvent(user){
-    var msg ={username: user, join: true};
-    $scope.msgs.push(msg);
+    displayMsg(msg);
   }
    
-  function join(){
-    $riffle.publish('exisChat', $riffle.user.username());
-    $scope.welcome = "Welcome " + $riffle.user.username() + "!";
-  }
-
-  $riffle.subscribe('exisChat', wantMsg);
-  $riffle.subscribe('exisChat', wantJoinEvent);
-
+  //publish message
   $scope.sendMsg = function(text){
     var msg = {username: $riffle.user.username(), msg: text}
     $riffle.publish('exisChat', msg);
+    $scope.input.msg = '';
+    displayMsg(msg);
+  }
+
+  //display our msg
+  function displayMsg(msg){
     $scope.msgs.push(msg);
-    $scope.input.msg = "";
-    $ionicScrollDelegate.$getByHandle('mainScroll').scrollBottom();
+    $ionicScrollDelegate.scrollBottom();
   }
 });
